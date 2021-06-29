aeca9e0d9a14fb1472c0acb6b7771ce86ec27825
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-06-29 14:41:02 +0700

    Update base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,51 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {en_US, NZ_I18N} from 'ng-zorro-antd/i18n';
-import {registerLocaleData} from '@angular/common';
+import { registerLocaleData, PathLocationStrategy, LocationStrategy } from '@angular/common';
 import en from '@angular/common/locales/en';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NZ_ICONS} from "ng-zorro-antd/icon";
-import {IconDefinition} from '@ant-design/icons-angular';
-import * as AllIcons from '@ant-design/icons-angular/icons';
-import {ScrollingModule} from '@angular/cdk/scrolling';
+
+import { AppRoutingModule } from './app-routing.module';
+import { TemplateModule } from './shared/template/template.module';
+import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { CommonLayoutComponent } from './layouts/common-layout/common-layout.component';
+import { FullLayoutComponent } from './layouts/full-layout/full-layout.component';
+
+import { NgChartjsModule } from 'ng-chartjs';
+import { ThemeConstantService } from './shared/services/theme-constant.service';
 
 registerLocaleData(en);
 
-const antDesignIcons = AllIcons as {
-  [key: string]: IconDefinition;
-};
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
-
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
-    ReactiveFormsModule,
-    ScrollingModule
-  ],
-  providers: [{provide: NZ_I18N, useValue: en_US}, {provide: NZ_ICONS, useValue: icons}],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CommonLayoutComponent,
+        FullLayoutComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        NzBreadCrumbModule,
+        TemplateModule,
+        SharedModule,
+        NgChartjsModule
+    ],
+    providers: [
+        { 
+            provide: NZ_I18N,
+            useValue: en_US, 
+        },
+        {
+            provide: LocationStrategy, 
+            useClass: PathLocationStrategy
+        },
+        ThemeConstantService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
