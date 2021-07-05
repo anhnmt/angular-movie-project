1615e8a491db40ee55be7bc6c14e971fc5d2b78e
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-07-05 20:31:49 +0700

    Update crud movie-type

--- src/app/dashboard/movie-type/movie-type-create/movie-type-create.component.ts
+++ src/app/dashboard/movie-type/movie-type-create/movie-type-create.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {StatusUtils} from '../../../shared/utils/statusUtils';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Subject} from 'rxjs';
+import {Router} from '@angular/router';
+import {MovieTypeService} from '../../../shared/services/movie-type.service';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {SharedService} from '../../../shared/services/shared.service';
 
 @Component({
   selector: 'app-movie-type-create',
   templateUrl: './movie-type-create.component.html',
   styleUrls: ['./movie-type-create.component.css']
 })
-export class MovieTypeCreateComponent implements OnInit {
+export class MovieTypeCreateComponent implements OnInit, AfterViewInit, OnDestroy {
+  visible = false;
+  status = StatusUtils.getDefaultStatus();
+  validateForm: FormGroup;
+  private onDestroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private readonly movieTypeService: MovieTypeService,
+    private formBuilder: FormBuilder,
+    private nzMessageService: NzMessageService,
+    private sharedService: SharedService,
+  ) {
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.visible = true;
+    }, 1);
+  }
+
+  close(): void {
+    this.visible = false;
+
+    setTimeout(
+      () => this.router.navigate(['/dashboard', 'movies', 'types']),
+      100
+    );
+  }
+
+  ngOnInit(): void {
+    const selectedStatus = this.status[0].value || null;
+
+    this.validateForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      slug: [null, [Validators.required]],
+      status: [selectedStatus, [Validators.required]],
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
+  }
+
+  submitForm(): void {
+    for (const key of Object.keys(this.validateForm.controls)) {
+      this.validateForm.controls[key].markAsDirty();
+      this.validateForm.controls[key].updateValueAndValidity();
+    }
+
+    // stop here if form is invalid
+    if (this.validateForm.invalid) {
+      return;
+    }
+
+    console.log(this.validateForm.value);
+
+    this.movieTypeService.createMovieType(this.validateForm.value).subscribe((success) => {
+      this.close();
+      this.sharedService.emitChange();
+      this.nzMessageService.success('Thêm Thành Công');
+    }, (error) => {
+      this.nzMessageService.error(error.message);
+    });
+  }
 }
--- src/app/dashboard/movie-type/movie-type-edit/movie-type-edit.component.ts
+++ src/app/dashboard/movie-type/movie-type-edit/movie-type-edit.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit} from '@angular/core';
+import {MovieType} from '../../../shared/interfaces/movie-type';
+import {StatusUtils} from '../../../shared/utils/statusUtils';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Subject} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MovieTypeService} from '../../../shared/services/movie-type.service';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {SharedService} from '../../../shared/services/shared.service';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-movie-type-edit',
   templateUrl: './movie-type-edit.component.html',
   styleUrls: ['./movie-type-edit.component.css']
 })
-export class MovieTypeEditComponent implements OnInit {
+export class MovieTypeEditComponent implements OnInit, AfterViewInit, OnDestroy {
+  visible = false;
+  movieType: MovieType;
+  status = StatusUtils.getDefaultStatus();
+  validateForm: FormGroup;
+  private onDestroy$: Subject<boolean> = new Subject<boolean>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private readonly route: ActivatedRoute,
+    private readonly movieTypeService: MovieTypeService,
+    private formBuilder: FormBuilder,
+    private nzMessageService: NzMessageService,
+    private sharedService: SharedService,
+  ) {
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.visible = true;
+    }, 1);
+  }
+
+  close(): void {
+    this.visible = false;
+
+    setTimeout(
+      () => this.router.navigate(['/dashboard', 'movies', 'types']),
+      100
+    );
+  }
+
+  ngOnInit(): void {
+    const selectedStatus = this.status[0].value || null;
+
+    this.validateForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      slug: [null, [Validators.required]],
+      status: [selectedStatus, [Validators.required]],
+    });
+
+    this.route.params.pipe(takeUntil(this.onDestroy$)).subscribe((params: any) => {
+      console.log(params);
+      const {movieTypeId} = params;
+      this.movieTypeService.getMovieTypeByMovieTypeId(movieTypeId).subscribe((success) => {
+        console.log(success);
+        this.movieType = success.data;
+
+        this.validateForm.patchValue({
+          name: this.movieType.name,
+          slug: this.movieType.slug,
+          status: this.movieType.status
+        });
+      });
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
+    this.onDestroy$.complete();
+  }
+
+  submitForm(): void {
+    for (const key of Object.keys(this.validateForm.controls)) {
+      this.validateForm.controls[key].markAsDirty();
+      this.validateForm.controls[key].updateValueAndValidity();
+    }
+
+    // stop here if form is invalid
+    if (this.validateForm.invalid) {
+      return;
+    }
+
+    console.log(this.validateForm.value);
+
+    this.movieTypeService.updateMovieTypeByMovieTypeId(this.movieType.movie_type_id, this.validateForm.value).subscribe((success) => {
+      this.close();
+      this.sharedService.emitChange();
+      this.nzMessageService.success('Cập nhật Thành Công');
+    }, (error) => {
+      this.nzMessageService.error(error.message);
+    });
+  }
 }
