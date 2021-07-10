880b7e9fc993ab1268b5d0ac6d420145a24a02db
Tuan Anh Nguyen Manh <xdorro@gmail.com>
2021-07-10 15:36:19 +0700

    Update client layout

--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ClientComponent} from './client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ClientComponent,
+    data: {
+      title: 'Dashboard ',
+      headerDisplay: 'none'
+    }
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ClientRoutingModule { }
+export class ClientRoutingModule {
+}
--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -2,9 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {ClientRoutingModule} from './client-routing.module';
+import { ClientComponent } from './client.component';
+import { DetailComponent } from './detail/detail.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ClientComponent, DetailComponent, SearchComponent],
   imports: [
     CommonModule,
     ClientRoutingModule
